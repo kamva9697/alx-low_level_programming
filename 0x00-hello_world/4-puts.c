98a41617d36e97a1e9cdd053b99f16398ea3afd0
kamva9697 <kamva9697@gmail.com>
2022-09-08 15:33:19 +0200

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+/**
+ * main - prints the required output
+ * Return: returns a zero for success completion
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
