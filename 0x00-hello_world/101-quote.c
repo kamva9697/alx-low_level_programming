c6b881fcf677b020eb43bfa7fd7325be1cc23516
kamva9697 <kamva9697@gmail.com>
2022-09-09 16:14:18 +0200

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+/**
+ * main - prints the required output
+ * Return: returns a zero for success completion
+ */
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
