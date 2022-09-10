75a43f95017aeaa61d2df0560bc77e5d2970635a
kamva9697 <kamva9697@gmail.com>
2022-09-11 01:00:40 +0200

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints all alphabets
+ * Return: zero (Success)
+ */
+int main(void)
+{
+	char c = ' ';
+
+	for (c = 'a'; c != 0x7B ; c++)
+		putchar(c);
+
+	for (c = 'A'; c != 0x5B ; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
