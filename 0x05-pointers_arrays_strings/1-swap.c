47374f104037cb3e47a74c195851d7fa7d4e033a
kamva9697 <kamva9697@gmail.com>
2022-09-24 19:46:58 +0200

    0x05: fix task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets a pointer to an int to 98
- *
- * @ptr: pointer to int
+ * swap_int - resets a pointer to an int to 98
+ * @a: pointer to int
+ * @b: pointer to int
  *
  */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c = *a;
 	*a = *b;
 	*b = c;
