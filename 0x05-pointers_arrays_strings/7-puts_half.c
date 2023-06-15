1c2b33d599ed11011c23c2267d034847e9b51def
kamva9697 <kamva9697@gmail.com>
2023-06-16 00:04:22 +0200

    0x05: Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of a string and a newline
+*
+* @str: The string to print
+*
+* Returns: nothing
+*/
+void puts_half(char *str)
+{
+	int strlen = 0;
+	int n;
+	int i;
+
+	while (str[strlen] != '\0')
+		strlen++;
+
+	if (strlen % 3 == 0)
+		n = (strlen - 1) / 2;
+	else
+		n = strlen / 2;
+
+	for (i = n; i < strlen; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void reset_to_98(int*);
 void swap_int(int *a, int *b);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif
