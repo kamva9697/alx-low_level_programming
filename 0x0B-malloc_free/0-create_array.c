b714cd4b18899c7015433fc7b3a40275d652fd42
kamva9697 <kamva9697@gmail.com>
2023-10-03 11:31:35 +0200

    Malloc_free: Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *buffer;
 
-	buffer = (char *) malloc(sizeof(size));
+	buffer = (char *) malloc(size);
 
 	for (i = 0; i < size; i++)
 	{
